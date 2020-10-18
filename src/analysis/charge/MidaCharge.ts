f582458f4e517e76ba5285640bc24425c77b9f79
Vasile Pește <vasile.peste@protonmail.ch>
2020-10-19 00:41:37 +0200

    Add work in progress.

--- src/analysis/charge/MidaCharge.ts
+++ src/analysis/charge/MidaCharge.ts
@@ -1,19 +1,19 @@
 import { MidaChargeType } from "#analysis/charge/MidaChargeType";
-import { MidaSecurityExchangeRate } from "#security/MidaSecurityExchangeRate";
+import {MidaForexPairExchangeRate} from "#forex/MidaForexPairExchangeRate";
 
 export class MidaCharge {
     // Represents the charge ticks.
-    private readonly _ticks: MidaSecurityExchangeRate[];
+    private readonly _ticks: MidaForexPairExchangeRate[];
 
     // Represents the charge type.
     private readonly _type: MidaChargeType;
 
-    public constructor (ticks: MidaSecurityExchangeRate[], type: MidaChargeType) {
+    public constructor (ticks: MidaForexPairExchangeRate[], type: MidaChargeType) {
         this._ticks = ticks;
         this._type = type;
     }
 
-    public get ticks (): readonly MidaSecurityExchangeRate[] {
+    public get ticks (): readonly MidaForexPairExchangeRate[] {
         return this._ticks;
     }
 
