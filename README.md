438d2780ed86a1dba40813dc550ac2b392513011
Vasile Pește <vasile.peste@protonmail.ch>
2020-10-21 01:27:56 +0200

    Add work in progress.

--- README.md
+++ README.md
@@ -3,26 +3,10 @@
 </p>
 <br>
 
-MidaFX is a framework designed to perform real time investments in the securities market. Designed to create expert advisors, perform analysis, manage portfolios, test
-and execute investments.
+MidaFX is an open source framework for real time exchange in markets such as stock, forex and commodities.
+Allows to easily execute, test and automate investment and trading strategies.
 
 ## Installation
 ```console
 npm install midafx
 ```
-
-## API
-Please refer to API.md.
-
-### Broker
-WIP.
-
-### How to listen the market
-WIP.
-
-### How to open a position
-WIP.
-
-### Playground Broker
-The playground broker is a local broker designed by MidaFX, acts as a real broker once time series are imported and it's
-mainly used to backtest investment strategies by passing it to an expert advisor.
--- src/analysis/charge/MidaCharge.ts
+++ src/analysis/charge/MidaCharge.ts
@@ -1,27 +1,31 @@
 import { MidaChargeType } from "#analysis/charge/MidaChargeType";
-import {MidaForexPairExchangeRate} from "#forex/MidaForexPairExchangeRate";
+import { MidaForexPairExchangeRate } from "#forex/MidaForexPairExchangeRate";
 
 export class MidaCharge {
-    // Represents the charge ticks.
-    private readonly _ticks: MidaForexPairExchangeRate[];
+    // Represents the charge quotations.
+    private readonly _quotations: MidaForexPairExchangeRate[];
 
     // Represents the charge type.
     private readonly _type: MidaChargeType;
 
-    public constructor (ticks: MidaForexPairExchangeRate[], type: MidaChargeType) {
-        this._ticks = ticks;
+    public constructor (quotations: MidaForexPairExchangeRate[], type: MidaChargeType) {
+        this._quotations = quotations;
         this._type = type;
     }
 
-    public get ticks (): readonly MidaForexPairExchangeRate[] {
-        return this._ticks;
+    public get quotations (): readonly MidaForexPairExchangeRate[] {
+        return this._quotations;
     }
 
     public get length (): number {
-        return this._ticks.length;
+        return this._quotations.length;
     }
 
     public get type (): MidaChargeType {
         return this._type;
+    }
+
+    public static calculate (quotations: MidaForexPairExchangeRate[], minDistance: number, threshold: number = 0): MidaCharge[] {
+        return [];
     }
 }
