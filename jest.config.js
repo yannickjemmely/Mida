6210dce325ccc0b460b80a297f53c57c1c88a7d4
Vasile Pește <vasile.peste@protonmail.ch>
2021-01-27 17:10:11 +0100

    Complete jest configuration.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const tsConfiguration = require("./tsconfig");
+
+const jestConfiguration = {
     testEnvironment: "node",
     coverageProvider: "v8",
     roots: [
@@ -8,7 +10,17 @@ module.exports = {
     testMatch: [
         "**/?(*.)+(spec|test).+(ts|tsx|js)"
     ],
-    moduleNameMapper: {
-        "^#periods/(.*)": __dirname + "/build/src/periods/$1"
-    }
+    moduleNameMapper: {},
 };
+
+Object.keys(tsConfiguration.compilerOptions.paths).forEach((key) => {
+    const normalizedAlias = "^" + (key.endsWith("/*") ? key.substr(0, key.length - 2) : key) + "/(.*)";
+
+    for (const plainPath of tsConfiguration.compilerOptions.paths[key]) {
+        const normalizedPath = plainPath.endsWith("/*") ? plainPath.substr(0, plainPath.length - 2) : plainPath;
+
+        jestConfiguration.moduleNameMapper[normalizedAlias] = `${__dirname}/${tsConfiguration.compilerOptions.outDir}/${normalizedPath}/$1`;
+    }
+});
+
+module.exports = jestConfiguration;
