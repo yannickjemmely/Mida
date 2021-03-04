df2a2b91bfc9ee8b9ff0e4bcb91565ed3dfac197
Vasile Pește <vasile.peste@protonmail.ch>
2021-03-04 02:20:38 +0100

    Add work in progress.

--- plugins/brokers/meta/MetaBroker.ts
+++ plugins/brokers/meta/MetaBroker.ts
@@ -1,7 +1,10 @@
+import puppeteer = require("puppeteer");
 import { MidaBroker } from "#brokers/MidaBroker";
 import { MidaBrokerAccount } from "#brokers/MidaBrokerAccount";
 
 export class MetaBroker extends MidaBroker {
+    private static readonly _WEB_META_TRADER_URI: string = "https://www.mql5.com/en/trading";
+
     public constructor (name: string) {
         super({
             name,
@@ -10,6 +13,20 @@ export class MetaBroker extends MidaBroker {
     }
 
     public async login (...parameters: any[]): Promise<MidaBrokerAccount> {
+        try {
+            const browser: any = await puppeteer.launch();
+            const metaTab: any = await browser.newPage();
+
+            await metaTab.goto(MetaBroker._WEB_META_TRADER_URI);
+        }
+        catch (error) {
+            throw new Error();
+        }
+
+        throw new Error();
+    }
+
+    private async _tryLoginFromTab (tab: any): Promise<boolean> {
         throw new Error();
     }
 }
