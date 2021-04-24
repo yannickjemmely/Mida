b9294b56ff4596d932ff2f4478a8a8e9a3e86cde
Vasile Pește <vasile.peste@protonmail.ch>
2021-04-24 15:49:53 +0200

    Remove aliases configuration for jest.

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,11 @@
-const tsConfiguration = require("./tsconfig");
-const jestConfiguration = {
+module.exports = {
     testEnvironment: "node",
     coverageProvider: "v8",
     roots: [
         "./build/tests/",
     ],
-    clearMocks: true,
     testMatch: [
         "**/?(*.)+(spec|test).+(ts|tsx|js)",
     ],
-    moduleNameMapper: {},
+    clearMocks: true,
 };
-
-Object.keys(tsConfiguration.compilerOptions.paths).forEach((key) => {
-    let normalizedAlias;
-
-    if (key.endsWith("/*")) {
-        normalizedAlias = "^" + key.substr(0, key.length - 2) + "/(.*)";
-    }
-    else if (key.endsWith("*")) {
-        normalizedAlias = "^" + key.substr(0, key.length - 1) + "(.*)";
-    }
-    else {
-        normalizedAlias = "^" + key + "/(.*)";
-    }
-
-    for (const plainPath of tsConfiguration.compilerOptions.paths[key]) {
-        const normalizedPath = plainPath.endsWith("/*") ? plainPath.substr(0, plainPath.length - 2) : plainPath;
-
-        jestConfiguration.moduleNameMapper[normalizedAlias] = `${__dirname}/${tsConfiguration.compilerOptions.outDir}/${normalizedPath}/$1`;
-    }
-});
-
-module.exports = jestConfiguration;
