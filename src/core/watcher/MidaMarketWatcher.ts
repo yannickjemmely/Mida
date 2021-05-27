547115187ca8d5e29398c8901fe981bf3219389d
Vasile Pește <vasile.peste@protonmail.ch>
2021-05-27 22:08:47 +0200

    Add market watcher work in progress.

--- src/core/watcher/MidaMarketWatcher.ts
+++ src/core/watcher/MidaMarketWatcher.ts
@@ -1,17 +1,20 @@
 import { MidaBrokerAccount } from "#brokers/MidaBrokerAccount";
+import { MidaEvent } from "#events/MidaEvent";
 import { MidaEmitter } from "#utilities/emitters/MidaEmitter";
 import { GenericObject } from "#utilities/GenericObject";
 
 /** WORK IN PROGRESS. */
 export class MidaMarketWatcher {
-    private readonly _brokerAccount?: MidaBrokerAccount;
-    private readonly _emitter?: MidaEmitter;
+    private readonly _brokerAccount: MidaBrokerAccount;
     private readonly _watchedTicks: Map<string, string>;
     private readonly _watchedPeriods: Map<string, GenericObject>;
+    private readonly _emitter: MidaEmitter;
 
-    public constructor () {
+    public constructor ({ brokerAccount, }: any) {
+        this._brokerAccount = brokerAccount;
         this._watchedTicks = new Map();
         this._watchedPeriods = new Map();
+        this._emitter = new MidaEmitter();
     }
 
     public watchTicks (symbol: string): void {
@@ -20,5 +23,23 @@ export class MidaMarketWatcher {
         if (this._watchedTicks.has(normalizedSymbol)) {
             return;
         }
+
+        this._watchedTicks.set(normalizedSymbol, this._brokerAccount.on("tick", (event: MidaEvent): void => {
+            if (event.descriptor.tick.symbol === normalizedSymbol) {
+                this.notifyListeners("tick", event.descriptor);
+            }
+        }) as string);
+    }
+
+    public unwatchTicks (symbol: string): void {
+        const uuid: string | undefined = this._watchedTicks.get(symbol.toUpperCase());
+
+        if (!uuid) {
+            return;
+        }
+    }
+
+    protected notifyListeners (type: string, descriptor?: GenericObject): void {
+        this._emitter.notifyListeners(type, descriptor);
     }
 }
