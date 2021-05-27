9a3847a755bb0eece020359eda0965fd09164489
Vasile Pește <vasile.peste@protonmail.ch>
2021-05-27 16:39:43 +0200

    Add market watcher work in progress.

--- src/core/watcher/MidaMarketWatcher.ts
+++ src/core/watcher/MidaMarketWatcher.ts
@@ -1,12 +1,24 @@
 import { MidaBrokerAccount } from "#brokers/MidaBrokerAccount";
 import { MidaEmitter } from "#utilities/emitters/MidaEmitter";
+import { GenericObject } from "#utilities/GenericObject";
 
+/** WORK IN PROGRESS. */
 export class MidaMarketWatcher {
     private readonly _brokerAccount?: MidaBrokerAccount;
     private readonly _emitter?: MidaEmitter;
-    private readonly _watchedSymbols: Map<string, string>;
+    private readonly _watchedTicks: Map<string, string>;
+    private readonly _watchedPeriods: Map<string, GenericObject>;
 
     public constructor () {
-        this._watchedSymbols = new Map();
+        this._watchedTicks = new Map();
+        this._watchedPeriods = new Map();
+    }
+
+    public watchTicks (symbol: string): void {
+        const normalizedSymbol: string = symbol.toUpperCase();
+
+        if (this._watchedTicks.has(normalizedSymbol)) {
+            return;
+        }
     }
 }
