771b39c2b164e5b2715a281c37fc2fc87b9095ef
Vasile Pește <vasile.peste@protonmail.ch>
2021-05-28 03:05:21 +0200

    Add market watcher work in progress.

--- src/core/watcher/MidaMarketWatcher.ts
+++ src/core/watcher/MidaMarketWatcher.ts
@@ -2,12 +2,13 @@ import { MidaBrokerAccount } from "#brokers/MidaBrokerAccount";
 import { MidaEvent } from "#events/MidaEvent";
 import { MidaEmitter } from "#utilities/emitters/MidaEmitter";
 import { GenericObject } from "#utilities/GenericObject";
+import { MidaSymbolTick } from "#ticks/MidaSymbolTick";
 
 /** WORK IN PROGRESS. */
 export class MidaMarketWatcher {
     private readonly _brokerAccount: MidaBrokerAccount;
-    private readonly _watchedTicks: Map<string, string>;
-    private readonly _watchedPeriods: Map<string, GenericObject>;
+    private readonly _watchedTicks: Map<string, boolean>;
+    private readonly _watchedPeriods: Map<string, Set<number>>;
     private readonly _emitter: MidaEmitter;
 
     public constructor ({ brokerAccount, }: any) {
@@ -15,31 +16,43 @@ export class MidaMarketWatcher {
         this._watchedTicks = new Map();
         this._watchedPeriods = new Map();
         this._emitter = new MidaEmitter();
+
+        this._configureListeners();
     }
 
-    public watchTicks (symbol: string): void {
+    public async watchTicks (symbol: string): Promise<void> {
         const normalizedSymbol: string = symbol.toUpperCase();
 
         if (this._watchedTicks.has(normalizedSymbol)) {
             return;
         }
 
-        this._watchedTicks.set(normalizedSymbol, this._brokerAccount.on("tick", (event: MidaEvent): void => {
-            if (event.descriptor.tick.symbol === normalizedSymbol) {
-                this.notifyListeners("tick", event.descriptor);
-            }
-        }) as string);
+        this._watchedTicks.set(normalizedSymbol, true);
     }
 
-    public unwatchTicks (symbol: string): void {
-        const uuid: string | undefined = this._watchedTicks.get(symbol.toUpperCase());
+    public async unwatchTicks (symbol: string): Promise<void> {
+        const normalizedSymbol: string = symbol.toUpperCase();
 
-        if (!uuid) {
-            return;
-        }
+        this._watchedTicks.delete(normalizedSymbol);
+    }
+
+    public async watchPeriods (symbol: string, timeframe: number): Promise<void> {
+        
     }
 
     protected notifyListeners (type: string, descriptor?: GenericObject): void {
         this._emitter.notifyListeners(type, descriptor);
+    }
+
+    private _onTick (tick: MidaSymbolTick): void {
+        const normalizedSymbol: string = tick.symbol.toUpperCase();
+
+        if (this._watchedTicks.has(normalizedSymbol)) {
+            this.notifyListeners("tick", { tick, });
+        }
+    }
+
+    private _configureListeners (): void {
+        this._brokerAccount.on("tick", (event: MidaEvent) => this._onTick(event.descriptor.tick));
     }
 }
