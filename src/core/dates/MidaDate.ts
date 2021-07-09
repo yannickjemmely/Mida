929a5a673d2dd39f15032ecb581a5568e177ca88
Vasile Pește <vasile.peste@protonmail.ch>
2021-07-09 18:32:17 +0200

    Prepare utility methods

--- src/core/dates/MidaDate.ts
+++ src/core/dates/MidaDate.ts
@@ -1,25 +1,54 @@
+import { MidaDateParameters } from "#dates/MidaDateParameters";
 import { IMidaCloneable } from "#utilities/cloneable/IMidaCloneable";
 
 export class MidaDate implements IMidaCloneable {
     readonly #date: Date;
 
-    public constructor (date?: MidaDate | Date | number) {
-        if (typeof date === "number") {
-            this.#date = new Date(date);
-        }
-        else if (date instanceof Date) {
-            this.#date = new Date(date);
-        }
-        else {
-            this.#date = new Date();
-        }
+    public constructor ({ date, }: MidaDateParameters) {
+        this.#date = new Date();
+    }
+
+    public get milliseconds (): number {
+        return this.#date.getUTCMilliseconds();
+    }
+
+    public get seconds (): number {
+        return this.#date.getUTCSeconds();
+    }
+
+    public get minutes (): number {
+        return this.#date.getUTCMinutes();
+    }
+
+    public get hours (): number {
+        return this.#date.getUTCHours();
+    }
+
+    public get weekDay (): number {
+        return this.#date.getUTCDay();
+    }
+
+    public get monthDay (): number {
+        return this.#date.getUTCDate();
+    }
+
+    public get month (): number {
+        return this.#date.getUTCMonth();
+    }
+
+    public toIsoString (): string {
+        return this.#date.toISOString();
+    }
+
+    public getMinutesDifference (date: MidaDate): number {
+        return -1;
+    }
+
+    public getDaysDifference (date: MidaDate): number {
+        return -1;
     }
 
     public clone (): MidaDate {
-        return new MidaDate(this);
-    }
-
-    public valueOf (): number {
-        return this.#date.valueOf();
+        return new MidaDate({ date: this, });
     }
 }
--- src/core/dates/MidaDateParameters.ts
+++ src/core/dates/MidaDateParameters.ts
@@ -1,5 +1,9 @@
 import { MidaDate } from "#dates/MidaDate";
 
+/**
+ * The date constructor parameters.
+ * @see MidaDate
+ */
 export type MidaDateParameters = {
-    
+    date?: Date;
 };
