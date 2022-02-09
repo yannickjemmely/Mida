43da7e7ef92bc83c44c06da6093872777b81f9d9
Vasile Pește <vasile.peste@protonmail.ch>
2022-02-09 23:13:47 +0100

    Cleanup+

--- src/core/indicators/MidaIndicatorIo.ts
+++ src/core/indicators/MidaIndicatorIo.ts
@@ -1,3 +1,4 @@
 import { GenericObject } from "#utilities/GenericObject";
 
+/** Represents the indicator input/output */
 export type MidaIndicatorIo = GenericObject | number;
--- src/core/indicators/MidaIndicatorParameters.ts
+++ src/core/indicators/MidaIndicatorParameters.ts
@@ -1,3 +1,9 @@
+import { MidaIndicator } from "#indicators/MidaIndicator";
+
+/**
+ * The indicator constructor parameters.
+ * @see MidaIndicator
+ */
 export type MidaIndicatorParameters = {
     name: string;
 };
