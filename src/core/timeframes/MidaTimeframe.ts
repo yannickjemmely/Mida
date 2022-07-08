579313facf10b5436ad6639d97a428b01e872c6b
Vasile Pește <vasile.peste@protonmail.ch>
2022-07-08 16:33:32 +0200

    Cleanup

--- src/core/timeframes/MidaTimeframe.ts
+++ src/core/timeframes/MidaTimeframe.ts
@@ -20,38 +20,6 @@
  * THE SOFTWARE.
 */
 
-export namespace MidaTimeframe {
-    const commonTimeframes: Map<string, number> = new Map([
-        [ "S", 1, ],
-        [ "M", 60, ],
-        [ "H", 3600, ],
-        [ "D", 86400, ],
-        [ "W", 604800, ],
-        [ "MO", 2592000, ],
-        [ "Y", 31536000, ],
-    ]);
-
-    export function parseTimeframe (timeframe: string): number | undefined {
-        const orderedTimeframes: string[] = [ ...commonTimeframes.keys(), ].sort((a: string, b: string) => a.length - b.length);
-        let quantity: number = NaN;
-
-        for (const commonTimeframe of orderedTimeframes) {
-            if (timeframe.startsWith(commonTimeframe)) {
-                // @ts-ignore
-                quantity = Number.parseInt(timeframe.substring(commonTimeframe.length), 10) * commonTimeframes.get(commonTimeframe);
-
-                break;
-            }
-        }
-
-        if (!Number.isFinite(quantity) || quantity < 0) {
-            return undefined;
-        }
-
-        return quantity;
-    }
-}
-
 export enum MidaTimeframe {
     /** 1 second */
     S1 = 1,
@@ -75,4 +43,35 @@ export enum MidaTimeframe {
     MO1 = 2592000,
     /** 1 year */
     Y1 = 31536000,
+}
+
+export namespace MidaTimeframe {
+    const commonTimeframes: Map<string, number> = new Map([
+        [ "S", 1, ],
+        [ "M", 60, ],
+        [ "H", 3600, ],
+        [ "D", 86400, ],
+        [ "W", 604800, ],
+        [ "MO", 2592000, ],
+        [ "Y", 31536000, ],
+    ]);
+
+    export const parseTimeframe = (timeframe: string): number | undefined => {
+        const orderedTimeframes: string[] = [ ...commonTimeframes.keys(), ].sort((a: string, b: string) => a.length - b.length);
+        let quantity: number = NaN;
+
+        for (const commonTimeframe of orderedTimeframes) {
+            if (timeframe.startsWith(commonTimeframe)) {
+                quantity = Number.parseInt(timeframe.substring(commonTimeframe.length), 10) * (commonTimeframes.get(commonTimeframe) ?? -1);
+
+                break;
+            }
+        }
+
+        if (!Number.isFinite(quantity) || quantity < 0) {
+            return undefined;
+        }
+
+        return quantity;
+    };
 }
